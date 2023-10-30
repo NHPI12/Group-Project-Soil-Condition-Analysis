a43fad600c772446bed8ae76d35febd0dace9e87
NHPI12 <124034451+NHPI12@users.noreply.github.com>
2023-10-30 21:33:33 +0700

    first commit

---
+++ soiCondition/app/src/main/java/vn/edu/usth/soicondition/MainActivity.java
@@ -0,0 +1,55 @@
+package vn.edu.usth.soicondition;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private FragmentAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout =findViewById(R.id.tabLayout);
+        viewPager2 =findViewById(R.id.viewPager2);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Humidity"));
+        tabLayout.addTab(tabLayout.newTab().setText("Temperature"));
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new FragmentAdapter(fragmentManager , getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
+
+    }
+}
---
+++ soiCondition/app/src/main/java/vn/edu/usth/soicondition/humidityFragment.java
@@ -0,0 +1,23 @@
+package vn.edu.usth.soicondition;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class humidityFragment extends Fragment {
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_humidity, container, false);
+    }
+}

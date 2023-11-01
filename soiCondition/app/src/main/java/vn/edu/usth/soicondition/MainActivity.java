2f84e912f05d081b191a3fe923337439ad6e9acf
NHPI12 <124034451+NHPI12@users.noreply.github.com>
2023-11-01 18:17:02 +0700

    fetch data from db

--- soiCondition/app/src/main/java/vn/edu/usth/soicondition/MainActivity.java
+++ soiCondition/app/src/main/java/vn/edu/usth/soicondition/MainActivity.java
@@ -1,17 +1,30 @@
 package vn.edu.usth.soicondition;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager2 viewPager2;
     private FragmentAdapter adapter;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout =findViewById(R.id.tabLayout);
         viewPager2 =findViewById(R.id.viewPager2);
-
         tabLayout.addTab(tabLayout.newTab().setText("Humidity"));
         tabLayout.addTab(tabLayout.newTab().setText("Temperature"));
 
@@ -50,6 +62,26 @@ public class MainActivity extends AppCompatActivity {
                 super.onPageScrollStateChanged(state);
             }
         });
+        temperatureFragment fragment = (temperatureFragment) getSupportFragmentManager().findFragmentById(R.id.temperature_view_fragment);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("sensor_data");
+        TextView temperatureView = fragment.getView().findViewById(R.id.temperature_view);
+
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            Integer value = snapshot.child("sensor_data/temperature").getValue(Integer.class);
+                            String Stringvalue = value.toString();
+                            temperatureView.setText(Stringvalue);
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(MainActivity.this, "Failed mf", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
+
 }
--- soiCondition/app/src/main/java/vn/edu/usth/soicondition/temperatureFragment.java
+++ soiCondition/app/src/main/java/vn/edu/usth/soicondition/temperatureFragment.java
@@ -7,14 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class temperatureFragment extends Fragment {
-
+    private TextView textView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_temperature, container, false);
+        View view =  inflater.inflate(R.layout.fragment_temperature, container, false);
+        textView = view.findViewById(R.id.temperature_view);
+        return view;
+    }
+    public void updateTextView(String newText){
+        if(textView!=null){
+            textView.setText(newText);
+        }
     }
 }

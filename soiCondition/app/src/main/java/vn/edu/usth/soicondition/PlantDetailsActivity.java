7f942071f09b456a8bfd9da2f601d58c83b6669a
NHPI12 <124034451+NHPI12@users.noreply.github.com>
2023-12-22 17:37:55 +0700

    Update plantDetailsActivity
    Change fragment back to activity

--- soiCondition/app/src/main/java/vn/edu/usth/soicondition/PlantDetailsActivity.java
+++ soiCondition/app/src/main/java/vn/edu/usth/soicondition/PlantDetailsActivity.java
@@ -1,27 +1,24 @@
 package vn.edu.usth.soicondition;
 
-import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class PlantDetailsFragment extends Fragment {
+public class PlantDetailsActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
@@ -29,14 +26,16 @@ public class PlantDetailsFragment extends Fragment {
     public ActionBarDrawerToggle actionBarDrawerToggle;
     private NavigationView navigationView;
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_plant_details, container, false);
-        recyclerView = view.findViewById(R.id.imageListRecycleView);
-        layoutManager = new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL,false);
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant_details);
+        recyclerView = findViewById(R.id.imageListRecycleView);
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
         recyclerView.setLayoutManager(layoutManager);
+
+
         List<Integer> imageList = Arrays.asList(
                 R.drawable.ic_thumbnail,
                 R.drawable.ic_thumbnail,
@@ -47,10 +46,8 @@ public class PlantDetailsFragment extends Fragment {
                 R.drawable.ic_thumbnail,
                 R.drawable.ic_thumbnail
         );
-        Plant_Details_Image_Recycle_Adapter adapter = new Plant_Details_Image_Recycle_Adapter(requireContext(), imageList);
+        Plant_Details_Image_Recycle_Adapter adapter = new Plant_Details_Image_Recycle_Adapter(this, imageList);
         recyclerView.setAdapter(adapter);
-        // Inflate the layout for this fragment
-        return view;
     }
 
 }

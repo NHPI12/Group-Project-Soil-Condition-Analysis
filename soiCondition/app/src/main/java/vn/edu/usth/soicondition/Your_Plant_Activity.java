d075fab67fb15d982e2c1d7efe3ce2bd0197262a
NHPI12 <124034451+NHPI12@users.noreply.github.com>
2024-01-05 23:12:47 +0700

    update search function

--- soiCondition/app/src/main/java/vn/edu/usth/soicondition/Your_Plant_Activity.java
+++ soiCondition/app/src/main/java/vn/edu/usth/soicondition/Your_Plant_Activity.java
@@ -3,6 +3,7 @@ package vn.edu.usth.soicondition;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -12,6 +13,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -172,5 +174,24 @@ public class Your_Plant_Activity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         startActivity(intent);
         //finish();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.search_bar_menu,menu);
+        MenuItem menuItem = menu.findItem(R.id.search_action_bar);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                YourPlantRecycleAdapter.filterList(newText);
+                return true;
+            }
+        });
+        return  super.onCreateOptionsMenu(menu);
     }
 }

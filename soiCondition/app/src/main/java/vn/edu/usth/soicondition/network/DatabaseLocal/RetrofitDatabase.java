34c991c04d022c93f142c0496aafd59fee96303b
DucQuyBI12-376 <125538353+DucQuyBI12-376@users.noreply.github.com>
2024-01-26 20:25:47 +0700

    change something

--- soiCondition/app/src/main/java/vn/edu/usth/soicondition/network/DatabaseLocal/RetrofitDatabase.java
+++ soiCondition/app/src/main/java/vn/edu/usth/soicondition/network/DatabaseLocal/RetrofitDatabase.java
@@ -9,12 +9,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import vn.edu.usth.soicondition.network.ApiServiceDatabase;
 
 public class RetrofitDatabase {
-    // private static final String BASE_URL = "http://172.31.99.32/codes/fetchdata.php/"; // USTH FLoor 4
+    //private static final String BASE_URL = "http://172.31.99.32/codes/fetchdata.php/"; // USTH FLoor 4 ICT Lab Guest
     //private static final String BASE_URL = "http://192.168.1.150/codes/fetchdata.php/"; // 40th floor Tan Hoang Minh 4016
-    private static final String BASE_URL = "http://192.168.68.113/codes/fetchdata.php/"; // Nha Anh Linh
+    //private static final String BASE_URL = "http://192.168.68.113/codes/fetchdata.php/"; // Nha Anh Linh
     //private static final String BASE_URL = "http://192.168.50.149/codes/fetchdata.php/"; // Home floor 4
     //private static final String BASE_URL = "http://172.20.10.3/codes/fetchdata.php/"; // Nhat Anh Hotspot
-    //private static final String BASE_URL = "http://10.10.15.132/codes/fetchdata.php/"; //USTH Student
+    private static final String BASE_URL = "http://10.10.13.85/codes/fetchdata.php/"; //USTH Student
     //private static final String BASE_URL = "http://192.168.68.105/codes/fetchdata.php/";
     private static Retrofit retrofit;
     public static Retrofit getRetrofitInstance() {
